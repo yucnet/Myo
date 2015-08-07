7292cca825b30c9f452f9d5e494a9895e4718e7b
shaabana <shaabana@gmail.com>
2015-08-06 22:00:24 -0400

    Switching feature extraction to be done in data collection end (as opposed to python script inside repository PythonScripts called classifyEmgData, which will now be used extensively for classification)

--- MyoDataCollector/MyoDataCollector/main.cpp
+++ MyoDataCollector/MyoDataCollector/main.cpp
@@ -13,15 +13,41 @@
 #include <cstdio>
 #include <chrono>
 #include <sstream>
+#include <unistd.h>
+#include <termios.h>
+#include <thread>
 
 // The only file that needs to be included to use the Myo C++ SDK is myo.hpp.
 #include <myo/myo.hpp>
 
-// Classes that inherit from myo::DeviceListener can be used to receive events from Myo devices. DeviceListener
-// provides several virtual functions for handling different kinds of events. If you do not override an event, the
-// default behavior is to do nothing.
-// Roll - x, pitch - y, yaw - z
+bool spacebarPressed = false;
+int num_channels = 8;
 
+
+class BufferToggle {
+private:
+	struct termios t;
+	
+public:
+	
+	/*
+	 * Disables buffered input
+	 */
+	void off(void){
+		tcgetattr(STDIN_FILENO, &t);	// get the current terminal I/O structure
+		t.c_lflag &= ~ICANON;			// Manipulate the flag bits to do what you want it to do
+		tcsetattr(STDIN_FILENO, TCSANOW, &t);	// Apply new settings
+	}
+	
+	/*
+	 * Enables buffered input
+	 */
+	void on(void){
+		tcgetattr(STDIN_FILENO, &t);		// get current terminal I/O structure
+		t.c_lflag |= ICANON;				// Manipulate the flag bits to do what we want it to do
+		tcsetattr(STDIN_FILENO, TCSANOW, &t);	// Apply new settings
+	}
+};
 
 // Armband data structure
 struct ArmBand {
@@ -58,10 +84,25 @@ struct ArmBand {
 	myo::Pose currentPose;
 };
 
+struct EmgData{
+	// Timestamp of event
+	std::chrono::milliseconds timestamp;
+	
+	// Raw emg data in 8 channels
+	int emg[8];
+};
+
+// Vector to hold all raw emg data
+std::vector<EmgData> raw_data;
+
+// Classes that inherit from myo::DeviceListener can be used to receive events from Myo devices. DeviceListener
+// provides several virtual functions for handling different kinds of events. If you do not override an event, the
+// default behavior is to do nothing.
+// Roll - x, pitch - y, yaw - z
 class DataCollector : public myo::DeviceListener {
 public:
     float roll, pitch, yaw;
-	
+	bool spacebarPressed = false;
     // Data file values
     std::string accelDataFileNameRight = "right forearm/accelerometer_50 Hz.csv";
     std::string accelDataFileNameLeft = "left forearm/accelerometer_50 Hz.csv";
@@ -232,7 +273,7 @@ public:
 		knownMyos[identifyMyo(myo)].yAccelRaw = accel[1];
 		knownMyos[identifyMyo(myo)].zAccelRaw = accel[2];
 		knownMyos[identifyMyo(myo)].accel_timestamp = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-		print(knownMyos[identifyMyo(myo)]);
+		printIMU(knownMyos[identifyMyo(myo)]);
 	}
 	
 	void onGyroscopeData(myo::Myo* myo, uint64_t timestamp, const myo::Vector3<float>& gyro){
@@ -243,31 +284,66 @@ public:
 		knownMyos[identifyMyo(myo)].yGyroRaw = gyro[1];
 		knownMyos[identifyMyo(myo)].zGyroRaw = gyro[2];
 		knownMyos[identifyMyo(myo)].gyro_timestamp = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-		print(knownMyos[identifyMyo(myo)]);
+		printIMU(knownMyos[identifyMyo(myo)]);
 	}
 	
 	void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg){
-		knownMyos[identifyMyo(myo)].emgTimestamp = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-		for(size_t i = 0; i < 8; ++i){
-			knownMyos[identifyMyo(myo)].emg[i] = static_cast<int>(emg[i]);
+		if(spacebarPressed){
+			knownMyos[identifyMyo(myo)].emgTimestamp = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
+			for(size_t i = 0; i < 8; ++i){
+				knownMyos[identifyMyo(myo)].emg[i] = static_cast<int>(emg[i]);
+			}
+			saveEMG(knownMyos[identifyMyo(myo)]);
 		}
+	}
+	
+	/*
+	 * Function to save emg data of inputted myo armband 
+	 * into master raw data vector
+	 * for later feature extraction
+	 */
+	void saveEMG(ArmBand myoBand){
+		EmgData data;
+		data.timestamp = myoBand.emgTimestamp;
+		for(int i = 0; i < num_channels; i++){
+			data.emg[i] = myoBand.emg[i];
+		}
+		raw_data.push_back(data);
+	}
+	
+	/*
+	 * Function to print emg data of inputted myo armband 
+	 * to file
+	 */
+	void printEMG(ArmBand myoBand){
+		std::string emgOutput = std::to_string(myoBand.emgTimestamp.count());
+		for(size_t i = 0; i < num_channels; ++i){
+			emgOutput += "," + std::to_string(myoBand.emg[i]);
+		}
+		emgOutput += "\n";
+		
+		if(myoBand.whichArm == myo::armRight){
+			emgDataFileRight << emgOutput;
+		} else if(myoBand.whichArm == myo::armLeft){
+			emgDataFileLeft << emgOutput;
+		}
+
 	}
 	
     // We define this function to print the current values that were updated by the on...() functions above.
     // This function also prints the x, y, and z (roll, pitch, and yaw, respectively) to a file.
-    void print(ArmBand myoBand)
+    void printIMU(ArmBand myoBand)
     {
         for(size_t i = 0; i < knownMyos.size(); ++i){
             // Find Myo corresponding to this arm
             myoBand = knownMyos[i];
 			
             // Clear the current line
-            std::cout << '\r';
+         //   std::cout << '\r';
 	
 			std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
 			
 			std::string timestamp = std::to_string(ms.count());
-			
 			
 			std::string accelOutput = std::to_string(myoBand.accel_timestamp.count()) + "," + std::to_string(myoBand.xAccelRaw) + "," + std::to_string(myoBand.yAccelRaw) + "," + std::to_string(myoBand.zAccelRaw) +
 				","	+ std::to_string((float)magnitude(myoBand.xAccelRaw, myoBand.yAccelRaw,	myoBand.zAccelRaw)) + "\n";
@@ -275,20 +351,13 @@ public:
 			std::string gyroOutput = std::to_string(myoBand.gyro_timestamp.count()) + "," + std::to_string(myoBand.xGyroRaw) + "," + std::to_string(myoBand.yGyroRaw) + "," + std::to_string(myoBand.zGyroRaw) +
 				"," + std::to_string((float)magnitude(myoBand.xGyroRaw, myoBand.yGyroRaw, myoBand.zGyroRaw)) + "\n";
 			
-			std::string emgOutput = std::to_string(myoBand.emgTimestamp.count());
-			for(size_t i = 0; i < 8; ++i){
-				emgOutput += "," + std::to_string(myoBand.emg[i]);
-			}
-			emgOutput += "\n";
 			
 			if(myoBand.whichArm == myo::armRight){
 				accelDataFileRight << accelOutput;
 				gyroDataFileRight << gyroOutput;
-				emgDataFileRight << emgOutput;
 			} else if(myoBand.whichArm == myo::armLeft){
 				accelDataFileLeft << accelOutput;
 				gyroDataFileLeft << gyroOutput;
-				emgDataFileLeft << emgOutput;
 			}
 			
             // Print out the orientation. Orientation data is always available, even if no arm is currently recognized.
@@ -297,7 +366,7 @@ public:
             << '[' << std::string(myoBand.zAccelRaw, '*') << std::string(18 - myoBand.zAccelRaw, ' ') << ']';
         */
             
-            if (myoBand.onArm) {
+        /*    if (myoBand.onArm) {
                 // Print out the lock state, the currently recognized pose, and which arm Myo is being worn on.
             
                 // Pose::toString() provides the human-readable name of a pose. We can also output a Pose directly to an
@@ -311,17 +380,130 @@ public:
             } else {
                 // Print out a placeholder for the arm and pose when Myo doesn't currently know which arm it's on.
                 std::cout << '[' << std::string(8, ' ') << ']' << "[?]" << '[' << std::string(14, ' ') << ']';
-            }
-            std::cout << identifyMyo(myoBand.myo);
+            }*/
+          //  std::cout << identifyMyo(myoBand.myo);
 
-            std::cout << std::flush;
+     //       std::cout << std::flush;
         }
     }
 };
 
+/*
+ * Function to extract the Mean Absolute Value
+ * feature set (one value for each channel, making it 8 values)
+ * input: empty array (size 8),
+ * output: same array populated with MAV of each channel
+ */
+
+void extractMAV(double *MAVs){
+	long L = raw_data[0].timestamp.count() - raw_data[raw_data.size()].timestamp.count();
+	int channel_values[8] = {0};
+	for(int i = 0; i < raw_data.size(); ++i){
+		for(int j = 0; j < num_channels; ++j){
+			channel_values[j] += std::abs(raw_data[i].emg[j]);
+		}
+	}
+	for(int i = 0; i < num_channels; ++i){
+		MAVs[i] = channel_values[i] / L;
+	}
+}
+
+/*
+ * Simple frequency measure representing the # of times that 
+ * a waveform changes polarity. Due to the possibility of noise
+ * induced zero crossings, a threshold must be defined. So,
+ * a pair of consecutive samples constitutes a zero crossing only 
+ * if their absolute difference exceeds both a noise threshold and 
+ * their absolute sum (i.e. they have different polarity).
+ * Threshold assumed to be *10* by default, but we can experiment
+ * with different values later.
+ */
+void extractZC(double *ZCs){
+	int T = 10;		// noise threshold
+	for(int i = 0; i < raw_data.size()-1; ++i){
+		for(int j = 0; j < num_channels; ++j){
+			if((raw_data[i].emg[j] - raw_data[i+1].emg[j]) > std::max((raw_data[i].emg[j] - raw_data[i+1].emg[j]), T)){
+				ZCs[j]++;
+			}
+		}
+	}
+}
+
+/*
+ * Function to extract the slope sign changes (SSCs) 
+ * for each channel in EMG data. SSC is the number of times
+ * that a waveform changes slope polarity. It may 
+ * provide additional frequency information about the
+ * signal. We will use the same Threshold as the ZC extraction
+ * method above. (Not sure of this works yet, untested)
+ */
+void extractSSC(double *SSCs){
+	int T = 10;
+	for(int i = 1; i < raw_data.size()-1; ++i){
+		for(int j = 0; j < num_channels; ++j){
+			if((raw_data[i].emg[j] > std::max(raw_data[i-1].emg[j], raw_data[i+1].emg[j])
+			   || raw_data[i].emg[j] < std::min(raw_data[i-1].emg[j], raw_data[i+1].emg[j]))
+			   && std::max((std::abs(raw_data[i+1].emg[j] - raw_data[i].emg[j])),
+						   (std::abs(raw_data[i].emg[j] - raw_data[i-1].emg[j]))) > T){
+				   SSCs[j]++;
+			}
+		}
+	}
+}
+
+/*
+ * Function to extract the waveform length, which is the total 
+ * length of the signal over the data window, which represents
+ * a combined measure of aptitude, frequency and duration
+ */
+
+void extractWL(double *WLs){
+	int channel_values[8] = {0};
+	for(int i = 1; i < raw_data.size(); ++i){
+		for(int j = 0; j < num_channels; ++j){
+			WLs[j] += std::abs(raw_data[i].emg[j] - raw_data[i-1].emg[j]);
+		}
+	}
+}
+
+/*
+ * Function to extract features from raw_data
+ * and input them into file.
+ * input is global variable raw_data
+ * output is features written into file
+ */
+void extractFeatures(){
+	double MAVs[8] = {0};
+	double ZCs[8] = {0};
+	double WLs[8] = {0};
+	double SSCs[8] = {0};
+	
+	extractMAV(MAVs);
+	extractZC(ZCs);
+	extractWL(WLs);
+	extractSSC(SSCs);
+}
+
+
+
+void toggleSpace(DataCollector* collector){
+	while(1){
+		if(std::getchar() == 32 || std::getchar() == ' '){
+			collector->spacebarPressed = !collector->spacebarPressed;
+			if(collector->spacebarPressed){
+				std::cout << "Begin Measurement" << std::endl;
+			} else {
+				std::cout << "End Measurement" << std::endl;
+				extractFeatures();
+			}
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
-    
+	BufferToggle bt;
+	bt.off();
     // We catch any exceptions that might occur below -- see the catch statement for more details.
     try {
 		// multiple myo container
@@ -339,16 +521,23 @@ int main(int argc, char** argv)
         // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
         // Hub::run() to send events to all registered device listeners.
         hub.addListener(&collector);
-        
+		
+		std::thread t1(toggleSpace, &collector);
+		
         // Finally we enter our main loop.
-        while (1) {
-            // In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
-            // In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
-            hub.run(20);
-            // After processing events, we call the print() member function we defined above to print out the values we've
-            // obtained from any events that have occurred.
+		
+		// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+		// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
+		while(1){
+		
+			hub.run(20);
+		}
+		
+           // After processing events, we call the print() member function we defined above to print out the values we've
+		// obtained from any events that have occurred.
         //    collector.print();
-        }
+
+		
         
         // If a standard exception occurred, we print out its message and exit.
     } catch (const std::exception& e) {
